da338a661b77ff20f6201eaf882535decc6e7095
Romanna <romannabric@gmail.com>
2025-09-18 22:15:05 +0200

    +++

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const API_KEY = '52315817-6546dbd007993710b423406a2';
+
+export function getImagesByQuery(query) {
+  return axios
+    .get('https://pixabay.com/api/', {
+      params: {
+        key: API_KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+      },
+    })
+    .then(response => {
+      return response.data;
+    });
+}
---
+++ src/js/render-functions.js
@@ -0,0 +1,58 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const lightbox = new SimpleLightbox('.gallery a');
+const loaderEl = document.querySelector('.loader');
+
+export function createGallery(container, images) {
+  const markup = [...images]
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `
+    <li class="gallery-item">
+    <a class="gallery-link" href="${largeImageURL}"><img class="gallery-image" src="${webformatURL}" alt="${tags}" /></a>
+    <ul class="content-list">
+    <li class="content-item">
+    <h3 class="content-header">Likes</h3>
+    <p class="content-value">${likes}</p> 
+    </li>
+    <li class="content-item">
+    <h3 class="content-header">Views</h3>
+    <p class="content-value">${views}</p>
+    </li>
+    <li class="content-item">
+    <h3 class="content-header">Comments</h3>
+    <p class="content-value">${comments}</p>
+    </li>
+    <li class="content-item">
+    <h3 class="content-header">Downloads</h3>
+    <p class="content-value">${downloads}</p>
+    </li>
+    </ul>
+    </li>
+`
+    )
+    .join('');
+
+  container.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
+}
+
+export function clearGallery(container) {
+  container.innerHTML = '';
+}
+
+export function showLoader() {
+  loaderEl.classList.remove('is-hidden');
+}
+
+export function hideLoader() {
+  loaderEl.classList.add('is-hidden');
+}
